124e5a10889b354bc3c7a2eaebfc83e6ed4ea9cb
Sheika <sheika02x2@gmail.com>
2026-06-30 14:03:42 +0400

    adding univercity

---
+++ University System ERD & Models/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace University_System_ERD___Models
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
