b7cda9814d01fff84040baf1703b09b419d3c3fe
Sheika <sheika02x2@gmail.com>
2026-07-06 16:02:50 +0400

    add new user in ecommerc project

--- E-Commerce System ERD & Models/Program.cs
+++ E-Commerce System ERD & Models/Program.cs
@@ -1,10 +1,58 @@
-﻿namespace E_Commerce_System_ERD___Models
+﻿using E_Commerce_System_ERD___Models.Models;
+using System.Reflection.Metadata;
+
+namespace E_Commerce_System_ERD___Models
 {
     
     public class Program
     {
        
         public static EcommerceContext context = new EcommerceContext();
+        //Register a New User 
+        public static void RegisterUser()
+        {
+            Console.WriteLine("\n=== Register New User ===");
+
+            Console.Write("Enter username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Enter email: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+           
+            string passwordHash = password;
+
+            Console.Write("Enter full name: ");
+            string fullName = Console.ReadLine();
+
+            Console.Write("Enter phone number (optional, press Enter to skip): ");
+            string phone = Console.ReadLine();
+
+            Console.Write("Enter address (optional, press Enter to skip): ");
+            string address = Console.ReadLine();
+
+            
+            User newUser = new User
+            {
+                username= username,
+                email= email,
+                passwordHash= passwordHash,
+                fullName=fullName,
+                phoneNumber=phone,
+                address=address,
+                registrationDate = DateTime.Now,
+                isActive = true
+            };
+
+            context.Users.Add(newUser);
+
+            context.SaveChanges();
+
+            //.userId is now populated with the DB-assigned Id
+            Console.WriteLine("New user ID: " + newUser.userId);
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
